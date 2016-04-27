d18773aea8edc6c4e1630949ecffff7bb7845e76
Manuel <manuel.alfaro.sierra@gmail.com>
2016-04-27 11:19:50 +0200

    Update VolumeControl.m

--- src/ios/VolumeControl.m
+++ src/ios/VolumeControl.m
@@ -2,6 +2,13 @@
 #import <AVFoundation/AVFoundation.h>
 #import <MediaPlayer/MediaPlayer.h>
 
+#define IS_IPHONE5 (([[UIScreen mainScreen] bounds].size.height-568)?NO:YES) 
+#define IS_OS_5_OR_LATER    ([[[UIDevice currentDevice] systemVersion] floatValue] >= 5.0)
+#define IS_OS_6_OR_LATER    ([[[UIDevice currentDevice] systemVersion] floatValue] >= 6.0)
+#define IS_OS_7_OR_LATER    ([[[UIDevice currentDevice] systemVersion] floatValue] >= 7.0)
+#define IS_OS_8_OR_LATER    ([[[UIDevice currentDevice] systemVersion] floatValue] >= 8.0)
+#define IS_OS_9_OR_LATER    ([[[UIDevice currentDevice] systemVersion] floatValue] >= 9.0)
+
 @interface VolumeControl : CDVPlugin {
     // Member variables go here.
     @private MPVolumeView *volumeView;
@@ -124,7 +131,7 @@
         volumeView.showsVolumeSlider=NO;
     }
     
-    #ifdef __IPHONE_8_0
+    #ifdef IS_OS_8_OR_LATER
       volumeView.alpha = (show == YES)? 1.0 : 0.01;
     #endif
     
