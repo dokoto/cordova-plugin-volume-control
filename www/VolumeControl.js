da6c2cabf9e65ca4d3aee6cd980fbe1526b31772
Manuel <manuel.alfaro.sierra@gmail.com>
2016-04-28 11:11:59 +0200

    Update VolumeControl.js

--- www/VolumeControl.js
+++ www/VolumeControl.js
@@ -10,33 +10,16 @@ function defaults(object, source) {
   return object;
 }
 
-exports.toggleMute = function( success, error) {
-  exec(success, error, 'VolumeControl', 'toggleMute', []);
-};
-
-exports.isMuted = function(success, error) {
-  exec(success, error, 'VolumeControl', 'isMuted', []);
-};
-
-exports.getVolume = function(success, error) {
-  exec(success, error, 'VolumeControl', 'getVolume', []);
-};
-
-exports.setVolume = function(volume, success, error) {
-  if(volume > 1) {
+exports.setVolumeAfterHideHUD = function(volume, success, error) {
+  if (volume > 1) {
     volume /= 100;
   }
-  exec(success, error, 'VolumeControl', 'setVolume', [volume * 1]);
+  exec(success, error, 'VolumeControl', 'setVolumeAfterHideHUD', [volume * 1]);
 };
 
-exports.getCategory = function(success, error) {
-  exec(success, error, 'VolumeControl', 'getCategory', []);
-};
-
-exports.hideVolume = function(success, error) {
-  exec(success, error, 'VolumeControl', 'hideVolume', []);
-};
-
-exports.showVolume = function(success, error) {
-  exec(success, error, 'VolumeControl', 'showVolume', []);
+exports.setVolumeBeforeShowHUD = function(volume, success, error) {
+  if (volume > 1) {
+    volume /= 100;
+  }
+  exec(success, error, 'VolumeControl', 'setVolumeBeforeShowHUD', [volume * 1]);
 };
